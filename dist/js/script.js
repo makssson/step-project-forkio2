8913aa211e29b0d80a64799453722ad18e1a70db
Максим Яремченко <yarmaxon@gmail.com>
2021-10-20 23:11:19 +0300

    Update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,12 +3,13 @@ const firstBurgerLine = document.querySelector('.burger__line--first');
 const secondBurgerLine = document.querySelector('.burger__line--second');
 const thirdBurgerLine = document.querySelector('.burger__line--third');
 const navMenu = document.querySelector('.nav__menu');
+const navItem = document.querySelector('.nav__menu-item');
 
 
 
 document.addEventListener('click', function() {
 
-    if (burgerLines.getAttribute('data-active') === null && event.target === burgerLines || 
+    if (event.target === navItem || event.target === burgerLines || 
     event.target === firstBurgerLine || event.target === secondBurgerLine || event.target === thirdBurgerLine) {
         
         firstBurgerLine.style.transform = 'rotate(44deg) translateY(11px)';
@@ -24,27 +25,11 @@ document.addEventListener('click', function() {
         navMenu.style.left = '3.5%';
         navMenu.style.transition = '1.5s';
 
-        burgerLines.setAttribute('data-active', 'active');
-        console.log(event.target);
         
-
-    } else if (event.target === navMenu) {
-        firstBurgerLine.style.transform = 'rotate(44deg) translateY(11px)';
-        firstBurgerLine.style.transition = '1.5s';
-    
-        secondBurgerLine.style.overflow = 'hidden';
-        secondBurgerLine.style.transform = 'translateX(-600%)';
-        secondBurgerLine.style.transition = '1.5s';
-    
-        thirdBurgerLine.style.transform = 'rotate(-45deg) translateY(-11px)';
-        thirdBurgerLine.style.transition = '1.5s';
-
-        navMenu.style.left = '3.5%';
-        navMenu.style.transition = '1.5s';
-
-        burgerLines.setAttribute('data-active', 'active');
-        console.log(event.target);
+        
     } else {
+        console.log(event.target);
+
         firstBurgerLine.removeAttribute('style');
         firstBurgerLine.style.transition = '1.5s';
 
@@ -57,7 +42,7 @@ document.addEventListener('click', function() {
         navMenu.style.left = '-200%';
         navMenu.style.transition = '1.5s';
 
-        burgerLines.removeAttribute('data-active');
+        
     }
      
 })
